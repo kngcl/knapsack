ec98a2966715f9e624a2c1f1c3a35bff775b1b0a
charles <ketchasso72@gmail.com>
2022-12-09 16:58:12 +0100

    remove semi colon

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,41 +7,40 @@ const done = document.querySelector('.done')
 const totalW = document.querySelector('.weight')
 const totalV = document.querySelector('.value')
 const capa = document.querySelector('.capacity')
-let knapSack = {
+const knapSack = {
   capacity: 0,
   weight: 0,
   value: 0,
   item: []
 }
 
-let items = [
+const items = [
   { Name: 'Computer Science Subjects', weight: 20, values: 10 },
   { Name: 'Operating System', weight: 20, values: 10 },
   { Name: 'Computer Networks', weight: 20, values: 10 },
   { Name: 'Data Structure', weight: 20, values: 10 },
   { Name: 'Algorithm', weight: 20, values: 10 },
   { Name: 'C programming', weight: 20, values: 10 },
-  { Name: 'JAVA', weight: 20, values: 10 },
-  (capacity = 0),
-];
+  { Name: 'JAVA', weight: 20, values: 10 }
+]
 
-let selectObject;
+let selectObject
 
-load.onclick = function reload() {
-  window.location.reload();
-};
+load.onclick = function reload () {
+  window.location.reload()
+}
 
-done.onclick = function disabled() {
-  knapSack.capacity = capi.value;
-  capi.disabled = true;
-};
+done.onclick = function disabled () {
+  knapSack.capacity = capi.value
+  capi.disabled = true
+}
 
-test.onclick = function knapSacks() {
-  if (knapSack.capacity == 0) {
-    alert('please enter the size basket');
+test.onclick = function knapSacks () {
+  if (knapSack.capacity === 0) {
+    alert('please enter the size basket')
   } else {
-    for (const i = 0; i < items.length; i++) {
-      if (slct1.value == items[i].Name) {
+    for (let i = 0; i < items.length; i++) {
+      if (slct1.value === items[i].Name) {
         selectObject = items[i]
         if (selectObject.weight + knapSack.weight <= knapSack.capacity) {
           knapSack.item.push(selectObject)
